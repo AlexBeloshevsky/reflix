d1cc244ca9d04257841abd3a81d93fc3d88036c9
Alex Beloshevsky <alexbeloshevsky@gmail.com>
2018-09-25 10:41:28 +0300

    created the routes strucutre

---
+++ src/components/Catalog.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Link} from 'react-router-dom';
+
+import '../styles/catalog.css'
+
+class Catalog extends Component {
+
+  render() {
+    return (
+      <div>
+        Catalog
+      </div>
+    )
+  }
+
+}
+
+export default Catalog;
